e5d7f9c5077c7b5a03e09e8dcff9a98acf7786b5
Cory Dunn <101128448+coryrdunn83@users.noreply.github.com>
2023-03-03 17:08:48 -0700

    Updated UiState, ViewModel; Added Alert Dialog;

--- app/src/main/java/com/coryrdunn/pscodeexercise/ui/home/HomeScreen.kt
+++ app/src/main/java/com/coryrdunn/pscodeexercise/ui/home/HomeScreen.kt
@@ -1,8 +1,10 @@
 package com.coryrdunn.pscodeexercise.ui.home
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,6 +21,26 @@ fun HomeScreen(
 ) {
     val uiState: HomeScreenUiState by viewModel.uiState.collectAsStateWithLifecycle()
 
+    if (uiState.showDialog) {
+        AlertDialog(
+            onDismissRequest = { viewModel.toggleDialog() },
+            confirmButton = {
+                Text(
+                    text = "Accept",
+                    modifier = Modifier.clickable {
+                        viewModel.toggleDialog()
+                    }
+                )
+            },
+            title = {
+                Text(text = uiState.selectedDriverShipment ?: "Shipment")
+            },
+            text = {
+                Text(text = "Would you like to accept this shipment?")
+            }
+        )
+    }
+
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
@@ -27,7 +49,7 @@ fun HomeScreen(
             uiState.dataBundle?.drivers?.let {
                 items(it) { driver ->
                     DriverCard(name = driver) {
-
+                        viewModel.toggleDialog()
                     }
                     Spacer(modifier = Modifier.height(5.dp))
                 }
--- app/src/main/java/com/coryrdunn/pscodeexercise/ui/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/coryrdunn/pscodeexercise/ui/home/HomeScreenViewModel.kt
@@ -5,8 +5,8 @@ import androidx.lifecycle.viewModelScope
 import com.coryrdunn.pscodeexercise.domain.model.DataBundle
 import com.coryrdunn.pscodeexercise.domain.repository.DataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,18 +14,49 @@ class HomeScreenViewModel @Inject constructor(
     dataRepository: DataRepository
 ): ViewModel() {
 
-    @OptIn(ExperimentalCoroutinesApi::class)
-    val uiState: StateFlow<HomeScreenUiState> = dataRepository.dataFlow.mapLatest { bundle ->
-        HomeScreenUiState(dataBundle = bundle)
+    private val _uiState = MutableStateFlow(HomeScreenUiState())
+    val uiState: StateFlow<HomeScreenUiState> = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            dataRepository.dataFlow.collectLatest { data ->
+                _uiState.update { currentState ->
+                    currentState.copy(dataBundle = data)
+                }
+            }
+        }
     }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(),
-            initialValue = HomeScreenUiState()
-        )
+
+    fun toggleDialog(driver: String? = null) {
+        _uiState.update { currentState ->
+            currentState.copy(
+                showDialog = !currentState.showDialog,
+                selectedDriver = driver
+            )
+        }
+    }
 
 }
 
 data class HomeScreenUiState(
-    val dataBundle: DataBundle? = null
-)
+    val dataBundle: DataBundle? = null,
+    val showDialog: Boolean = false,
+    val selectedDriver: String? = null
+) {
+    val driverList: List<String>
+        get() = dataBundle?.drivers ?: emptyList()
+
+    val shipmentList: List<String>
+        get() = dataBundle?.shipments ?: emptyList()
+
+    val selectedDriverShipment: String?
+        get() {
+            val ssScoreMap = mutableMapOf<Int, String>()
+
+            shipmentList.forEach {
+                
+            }
+
+            return dataBundle?.shipments?.firstOrNull()
+        }
+}
